2c91920d18491b25bba9828b407bdf9024baff4d
GermanHernandez2902 <german.hernandez2902@gmail.com>
2025-09-30 02:28:32 -0500

    Preparando deploy limpio a GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Ejercicio1 from "./ejercicios/Ejercicio1";
 import Ejercicio2 from "./ejercicios/Ejercicio2";
 import Ejercicio3 from "./ejercicios/Ejercicio3";
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ejercicios-reactjs/',   // 👈 OBLIGATORIO para GitHub Pages
+  base: '/ejercicios-reactjs/', // 👈 para GitHub Pages
 })
