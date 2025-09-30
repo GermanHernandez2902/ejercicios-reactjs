69844a46c8953700892a66f8e5dd013173d76473
GermanHernandez2902 <german.hernandez2902@gmail.com>
2025-09-30 01:59:06 -0500

    fix: added base path for github pages in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ejercicios-reactjs/',
+  base: '/ejercicios-reactjs/',   // 👈 OBLIGATORIO para GitHub Pages
 })
